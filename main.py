8c4cd0a03470e8da64d1bcc4f2a6ebaa26613aef
KushSri2014 <156007613+KushSri2014@users.noreply.github.com>
2024-01-08 23:55:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import streamlit as st
+from PIL import Image
+import wikipedia
+import time
+st.title("Hi 🙌")
+st.text("Hello Welcome to this website:")
+go = st.text_input("What do you want to search?\n")
+time.sleep(2)
+su = wikipedia.summary(go)
+st.write(su)
+
+st.subheader("Simplest Python code to make an ai")
+st.code("""import wikipedia as wk
+ask = input("Enter anything:\n")
+summary = wk.summary(ask, sentences='3')
+print("summary")""")
